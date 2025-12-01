80caa487d73ed02277b19029e4d7c58bda190cf6
ChiLLLix-hub <chilllix.me@gmail.com>
2025-12-01 15:20:33 +0800

    Add files via upload

---
+++ okokCrafting/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'okok#3488'
+description 'okokCrafting'
+
+ui_page 'web/ui.html'
+
+files {
+	'web/*.*',
+	'web/icons/*.png'
+}
+
+shared_script 'config.lua'
+
+client_scripts {
+	'client.lua',
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server.lua'
+}
+
+--[[
+[#] Shared/Leaked by: 𝒆𝒛𝒛 𝒈𝒊𝒓𝒍𝒛#4084 / https://discord.gg/GyGVuHy35A
+[#] If you are spreading this script do not delete this file, It is not a request but a requirement!
+--]]
