48688fc76d26ba17cc21f493dc2af9f2df9d9111
ChiLLLix-hub <chilllix.me@gmail.com>
2026-02-21 16:11:04 +0800

    Update client.lua

--- okokNotify/client.lua
+++ okokNotify/client.lua
@@ -14,7 +14,7 @@ AddEventHandler('okokNotify:Alert', function(title, message, time, type)
 	Alert(title, message, time, type)
 end)
 
--- Example Commands - Delete them
+--[[ Example Commands - Delete them
 
 RegisterCommand('success', function()
 	exports['okokNotify']:Alert("SUCCESS", "You have sent <span style='color:#47cf73'>420€</span> to Tommy!", 5000, 'success')
@@ -38,4 +38,4 @@ end)
 
 RegisterCommand('longtext', function()
 	exports['okokNotify']:Alert("LONG MESSAGE", "Lorem ipsum dolor sit amet, consectetur adipiscing elit e pluribus unum.", 5000, 'neutral')
-end)
+end)]]
